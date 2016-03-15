c5a02e40464ec82c1868c06699b2c5b6a3902589
Alex Fallenstedt and Matt Knutson <mknut001@gmail.com>
2016-03-15 15:01:41 -0700

    able to display temp in farenheit

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -5,7 +5,14 @@ $(document).ready(function() {
     var city = $('#location').val();
     $('#location').val("");
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+      console.log(response);
+
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%" );
+
+      var tempF = Math.round(response.main.temp * (9/5) - 459.67);
+
+      $('.showTemperature').text("The temp in " + city + " is " + tempF + " degrees F" );
     });
+
   });
 });
