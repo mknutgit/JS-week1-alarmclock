0e7890f8ae9dc402930ea8735976968499f13498
Alex Fallenstedt and Matt Knutson <student@epicodus.com>
2016-03-15 09:25:16 -0700

    add template file

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,7 @@ var lib = require('bower-files')({
     }
   }
 });
+var browserSync = require('browser-sync').create();
 var buildProduction = utilities.env.production;
 
 gulp.task('concatInterface', function() {
@@ -72,3 +73,22 @@ gulp.task('bowerCSS', function () {
 });
 
 gulp.task('bower', ['bowerJS', 'bowerCSS']);
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+  gulp.watch(['js/*.js'], ['jsBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+});
+
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browserSync.reload();
+});
+
+gulp.task('bowerBuild', ['bower'], function(){
+  browserSync.reload();
+});
