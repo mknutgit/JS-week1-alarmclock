2d60069df3c900e6a4ee3804c57c13e3f8b7b5ba
Alex Fallenstedt and Matt Knutson <a.fallenstedt@gmail.com>
2016-03-15 14:19:38 -0700

    will get humidity from wherever you are

---
+++ js/weather-interface.js
@@ -0,0 +1,11 @@
+var apiKey = "dbf2c9049b26489186f49538b6d32c90";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    });
+  });
+});
